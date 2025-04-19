e81413c025701c89a9303e9153176b4de852d2eb
Phil Mass <philrmass@gmail.com>
2025-04-19 14:23:10 -0700

    update config for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/junco/',
   plugins: [preact(), VitePWA({
     registerType: 'prompt',
     injectRegister: false,
