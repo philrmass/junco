e69071b15e61a6a7dfc693834c4b64edcfc1e821
Phil Mass <philrmass@gmail.com>
2025-04-19 12:51:18 -0700

    add initial files

---
+++ src/app.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'preact/hooks'
+import preactLogo from './assets/preact.svg'
+import appLogo from '/favicon.svg'
+import PWABadge from './PWABadge.jsx'
+import './app.css'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={appLogo} class="logo" alt="junco logo" />
+        </a>
+        <a href="https://preactjs.com" target="_blank">
+          <img src={preactLogo} class="logo preact" alt="Preact logo" />
+        </a>
+      </div>
+      <h1>junco</h1>
+      <div class="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/app.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p>
+        Check out{' '}
+        <a
+          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
+          target="_blank"
+        >
+          create-preact
+        </a>
+        , the official Preact + Vite starter
+      </p>
+      <p class="read-the-docs">
+        Click on the Vite and Preact logos to learn more
+      </p>
+      <PWABadge />
+    </>
+  )
+}
---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [preact(), VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'junco',
+      short_name: 'junco',
+      description: 'A streaming music PWA',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
