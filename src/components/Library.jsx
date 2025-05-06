cb1961ea61fa58f9847dcdae33bada9e5f52e08e
Phil Mass <philrmass@gmail.com>
2025-05-05 20:20:22 -0700

    fix library styling

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -12,46 +12,50 @@ export default function Library({
   };
 
   return (
-    <div className={styles.library}>
-      { artists.map((artist) => (
-        <>
-          <div
-            className={styles.artist}
-            onClick={() => onCommand('toggleArtistIndex', artist.guid)}
-          >
-            { artist.name }
-          </div>
-          { artist.guid === artistGuid && (
-            <div>
-              { artist.albums.map((album) => (
-                <>
-                  <div
-                    className={styles.album}
-                    onClick={() => onCommand('toggleAlbumIndex', album.guid)}
-                  >
-                    <button onClick={(e) => handleAdd(e, 'addAlbum', album)}>
-                      +
-                    </button>
-                    { album.title }
-                  </div>
-                  { album.guid === albumGuid && (
-                    <div>
-                      { album.songs.map((song) => (
-                        <div
-                          className={styles.song}
-                          onClick={(e) => handleAdd(e, 'addSong', song)}
-                        >
-                          { song.title }
-                        </div>
-                      )) }
-                    </div>
-                  ) }
-                </>
-              )) }
+    <div className={styles.window}>
+      <div className={styles.topShadow} />
+      <div className={styles.library}>
+        { artists.map((artist) => (
+          <>
+            <div
+              className={styles.artist}
+              onClick={() => onCommand('toggleArtistIndex', artist.guid)}
+            >
+              { artist.name }
             </div>
-          ) }
-        </>
-      ))}
+            { artist.guid === artistGuid && (
+              <div>
+                { artist.albums.map((album) => (
+                  <>
+                    <div
+                      className={styles.album}
+                      onClick={() => onCommand('toggleAlbumIndex', album.guid)}
+                    >
+                      <button onClick={(e) => handleAdd(e, 'addAlbum', album)}>
+                        +
+                      </button>
+                      { album.title }
+                    </div>
+                    { album.guid === albumGuid && (
+                      <div>
+                        { album.songs.map((song) => (
+                          <div
+                            className={styles.song}
+                            onClick={(e) => handleAdd(e, 'addSong', song)}
+                          >
+                            { song.title }
+                          </div>
+                        )) }
+                      </div>
+                    ) }
+                  </>
+                )) }
+              </div>
+            ) }
+          </>
+        )) }
+      </div>
+      <div className={styles.bottomShadow} />
     </div>
   );
 }
--- src/components/Queue.jsx
+++ src/components/Queue.jsx
@@ -6,20 +6,22 @@ export default function Queue({
   onCommand,
 }) {
   return (
-    <div className={styles.queue}>
-      { queue.map((song, index) => (
-        <div className={`${styles.entry} ${index === queueIndex ? styles.selected : ''}`}>
-          <div className={styles.song}>
-            { song.title }
+    <div className={styles.main}>
+      <div className={`shadows ${styles.queue}`}>
+        { queue.map((song, index) => (
+          <div className={`${styles.entry} ${index === queueIndex ? styles.selected : ''}`}>
+            <div className={styles.song}>
+              { song.title }
+            </div>
+            <div className={styles.artist}>
+              { `${song.artist} / ${song.album}` }
+            </div>
           </div>
-          <div className={styles.artist}>
-            { `${song.artist} / ${song.album}` }
-          </div>
-        </div>
-      )) }
-      { queue.length === 0 && (
-        <div>No songs in queue</div>
-      ) }
+        )) }
+        { queue.length === 0 && (
+          <div>No songs in queue</div>
+        ) }
+      </div>
     </div>
   );
 }
